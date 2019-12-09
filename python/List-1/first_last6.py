a37ddf201e2dd6e9cd2015420f90514520a53174
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-09 12:44:47 +0900

    Added List-1

---
+++ python/List-1/first_last6.py
@@ -0,0 +1,6 @@
+'''Given an array of ints, return True if 6 appears as either the first or
+last element in the array. The array will be length 1 or more.'''
+
+
+def first_last6(nums):
+    return 6 in [nums[0], nums[-1]]
